7f7d1fbc798d6d794a786825218a44757a1a7ff4
MattZK <mattwillemsen@icloud.com>
2019-01-12 21:51:22 +0100

    Made Input Manager Singelton Class

--- Managers/InputManager.cs
+++ Managers/InputManager.cs
@@ -3,5 +3,18 @@ namespace GDPlatformer.Managers
 {
   public class InputManager
   {
+    private static InputManager instance;
+
+    public static InputManager Instance
+    {
+      get
+      {
+        if (instance == null)
+        {
+          instance = new InputManager();
+        }
+        return instance;
+      }
+    }
   }
 }
