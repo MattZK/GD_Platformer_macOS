921da74356bb8b6f4c252a0f85a0e501c4841ce9
MattZK <mattwillemsen@icloud.com>
2019-01-12 20:27:47 +0100

    Added override functions

--- Screens/GameScreen.cs
+++ Screens/GameScreen.cs
@@ -1,7 +1,30 @@
 ﻿using System;
+using GDPlatformer.Managers.Base;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
 namespace GDPlatformer.Screens
 {
-  public class GameScreen
+  public class GameScreen: Screen
   {
+    public override void LoadContent()
+    {
+      base.LoadContent();
+    }
+
+    public override void UnloadContent()
+    {
+      base.UnloadContent();
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+      base.Update(gameTime);
+    }
+
+    public override void Draw(SpriteBatch spriteBatch)
+    {
+      base.Draw(spriteBatch);
+    }
   }
 }
